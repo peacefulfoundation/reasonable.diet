234ad0c2fbbd0bb2b10389b101ad2cc08ecf9c19
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-07-07 08:55:36 +0530

    feat(ui): new icons and socials

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,17 @@
-import type { Links, Site, Socials } from '@/types';
+import type { Links, Page, Site, Socials } from '@/types';
 
+// Global
 export const SITE: Site = {
   TITLE: 'reasonable.diet',
   DESCRIPTION: 'Budget-friendly recipes for university students.',
   AUTHOR: 'peacefulfoundation.org',
 };
+
+// Search
+export const SEARCH: Page = {
+  TITLE: 'Search',
+  DESCRIPTION: 'Search for your favorite budget-friendly recipes!'
+}
 
 // Links
 export const LINKS: Links = [
@@ -25,5 +32,11 @@ export const SOCIALS: Socials = [
     ICON: 'github',
     TEXT: 'peacefulfoundation',
     HREF: 'https://github.com/peacefulfoundation/reasonable.diet'
+  },
+  {
+    NAME: 'Discord',
+    ICON: 'discord',
+    TEXT: 'quiteasily.org',
+    HREF: 'https://discord.gg/uvYhwtznTK',
   }
 ]
