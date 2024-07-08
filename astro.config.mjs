d0a686716e714cdd760d3af98fe58fe86bb2f2d2
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-07-08 08:44:36 +0530

    feat(ui): add main recipes page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://diet.abhigyantrips.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false })],
+  redirects: {
+    '/recipes': '/recipes/1',
+  },
 });
