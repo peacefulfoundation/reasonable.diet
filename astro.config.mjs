b50c647fbcb1f37c7831832d783c14db3694883c
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-07-13 21:23:23 +0530

    chore(deps): add solid and fuse.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
+import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://reasonable-diet.abhigyantrips.dev',
-  integrations: [tailwind({ applyBaseStyles: false }), mdx(), sitemap()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+    sitemap(),
+    solidJs({ devtools: true }),
+  ],
   redirects: {
     '/recipes': '/recipes/1',
     '/cook': '/recipes/1',
